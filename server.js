09d39494329a6eee84487d68d0ab431a4d573d51
Ryan Trinh <149514022+NguyenAnh28@users.noreply.github.com>
2026-09-22 15:11:50 -0400

    Added info route, updated package and package-lock (#7)
    * Added info route, updated package and package-lock
    
    * removed redundant duplicated info route

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ app.get("/", (req, res) => {
   res.send("<h1>Group 16</h1>");
 });
 
+app.get("/info", (req, res) => {
+  res.send(`
+    <h1>Product Information</h1>
+    <p>Group 16 is building a web app for the Cape Abilities Farm greenhouses.</p>
+    <p>It allows for employees and managers to see updated information on seeds.</p>
+`);
+});
+
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
